e89d283eb7689a9ebab61326c29ffc66bc21219f
Marie Eckert <mec@os-cillation.de>
2023-12-04 16:25:47 +0100

    finish aoc2 impl

--- 2/aoc2.pas
+++ 2/aoc2.pas
@@ -6,30 +6,38 @@ uses SysUtils, StrUtils, Types;
 {$H+}
 
 const
-  REDS = 0;
-  GREENS = 0;
-  BLUES = 0;
+  REDS = 12;
+  GREENS = 13;
+  BLUES = 14;
 
 function process(_str: String): Integer;
 var
-  ix, game_id: Integer;
-  sets: array of TStringDynArray;
-  tmp: TStringDynArray;
-  _set: String;
+  ix, ix_2, n, game_id: Integer;
+  tmp, tmp_2, tmp_3: TStringDynArray;
 begin
   tmp := SplitString(Copy(
     _str,
     pos(':', _str) + 2,
     Length(_str)
     ), ';');
+  
+  { Extract game id }
+  game_id := StrToInt(Copy(_str, pos(' ', _str), pos(':', _str) - pos(' ', _str)));
 
-  SetLength(sets, Length(tmp));
-  for ix := 0 to Length(sets) - 1 do
+  for ix := 0 to Length(tmp) - 1 do
   begin
-    sets[ix] := SplitString(Trim(tmp[ix]), ',');
-    writeln(Trim(tmp[ix]));
+    tmp_2 := SplitString(Trim(tmp[ix]), ',');
+    for ix_2 := 0 to Length(tmp_2) - 1 do
+    begin
+      tmp_3 := SplitString(Trim(tmp_2[ix_2]), ' ');
+      n := StrToInt(tmp_3[0]);
+      if (tmp_3[1] = 'blue') and (n > BLUES) then exit;
+      if (tmp_3[1] = 'red') and (n > REDS) then exit;
+      if (tmp_3[1] = 'green') and (n > GREENS) then exit;
+    end;
   end;
-  writeln('end');
+
+  process := game_id;
 end;
 
 var
@@ -37,7 +45,7 @@ var
   inval: String;
   out: Integer;
 begin
-  Assign(in_fl, 'test_input');
+  Assign(in_fl, 'input');
   ReSet(in_fl);
 
   out := 0;
